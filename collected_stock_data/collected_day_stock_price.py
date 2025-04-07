1dcbd3cbc3b3ea86953d5f25985076f4a85d9761
camellia-ver <jakahi435@gmail.com>
2025-04-07 20:54:28 +0900

    사용하지 않은 코드 삭제

--- collected_stock_data/collected_day_stock_price.py
+++
@@ -1,5 +0,0 @@
-from pykrx.stock import get_market_ohlcv_by_ticker
-from datetime import datetime, timedelta
-
-yesterday = (datetime.today() - timedelta(days=1)).strftime('%Y%m%d')
-df = get_market_ohlcv_by_ticker(date=yesterday, market="ALL")
