b1563c53729ec3df6658a4e1f64e82f8e7d94a4d
camellia-ver <jakahi435@gmail.com>
2025-04-07 20:37:59 +0900

    주식 데이터 DB저장 기능 추가

--- collected_stock_data/collected_realtime_stock_price.py
+++ collected_stock_data/collected_realtime_stock_price.py
@@ -6,8 +6,10 @@ import time
 import pandas as pd
 import json
 import os
-
-ERROR_LOG_PATH = f"data/logs/get_price_function_error_codes_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.txt"
+import pymysql
+from pymysql import cursors
+from dotenv import load_dotenv
+from typing import List,Dict
 
 def get_current_price(code:str) -> int:
     try:
@@ -34,8 +36,10 @@ def get_current_price(code:str) -> int:
 
     return None
 
-def write_error_code(code:str):
-    with open(ERROR_LOG_PATH,'a', encoding="utf-8") as f:
+def write_error_code(code:str,base_path="data/logs"):
+    date_str = now_str('%Y-%m-%d')
+    filepath = os.path.join(base_path, f"get_price_function_error_codes_{date_str}.txt")
+    with open(filepath,'a', encoding="utf-8") as f:
         f.write(f"{code}\n")
 
 def create_stock_data(codes:dict, duration_minutes:int):
@@ -43,10 +47,10 @@ def create_stock_data(codes:dict, duration_minutes:int):
     all_data = []
 
     for i in range(duration_minutes): 
+        next_time = datetime.now()
         print(f"\n========== {i+1}/{duration_minutes}분 수집 시작 ==========")
-        start_time = time.time()
 
-        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        now = now_str("%Y-%m-%d %H:%M:%S")
         print(f"\n[{now}] 수집시작")
 
         minute_data = []
@@ -75,8 +79,13 @@ def create_stock_data(codes:dict, duration_minutes:int):
             print("⚠️ 수집 시간이 1분을 초과했습니다.")
     
     all_data_df = pd.DataFrame(all_data)
-    now_str = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    now_str = now_str('%Y-%m-%d_%H-%M-%S')
     all_data_df.to_csv(f"data/prices/stock_price_{now_str}.csv", index=False, encoding='utf-8-sig')
+
+    db_connect = connect_db()
+    if db_connect:
+        save_stock_data(all_data, db_connect)
+        db_connect.close()
 
 def load_or_create_name_dict(filepath="data/name_dict.json") -> dict:
     if os.path.exists(filepath):
@@ -96,17 +105,50 @@ def get_name_dict():
         codes = stock.get_market_ticker_list(datetime.today().strftime('%Y%m%d'), market=market)
         for code in codes:
             name_dict[code] = stock.get_market_ticker_name(code)
-            time.sleep(0.01)
+            time.sleep(0.1)
     
     return name_dict
 
-kospi_codes = stock.get_market_ticker_list(datetime.today().strftime('%Y%m%d'), market="KOSPI")
-kosdaq_codes = stock.get_market_ticker_list(datetime.today().strftime('%Y%m%d'), market="KOSDAQ")
+def connect_db() -> pymysql.connections.Connection:
+    try:
+        load_dotenv()
 
-codes_dict = {
-    "KOSPI" : kospi_codes,
-    "KOSDAQ" : kosdaq_codes
-}
-duration_minutes = 2 #60 * 24 # 실행하고 싶은 시간(단위:분)
+        db_connect = pymysql.connect(
+            user=os.getenv("DB_USER"),
+            passwd=os.getenv("DB_PASSWORD"),
+            host=os.getenv("DB_HOST"),
+            db=os.getenv("DB_NAME"),
+            charset='utf8'
+        )
 
-create_stock_data(codes_dict,duration_minutes)
+        return db_connect
+    except Exception as e:
+        print(f"DB 연결 실패: {e}")
+        return None
+
+def save_stock_data(datas: List[Dict], db_connect: pymysql.connections.Connection):
+    cursor = db_connect.cursor(cursors.DictCursor)
+
+    insert_query = """INSERT INTO stock(code, name, price, time, market) VALUES(%s, %s, %s, %s, %s)"""
+    values = [(data['종목코드'],data['종목명'],data['현재가'],data['시간'],data['구분']) for data in datas]
+    cursor.executemany(insert_query, values)
+
+    db_connect.commit()
+
+def now_str(fmt='%Y-%m-%d %H:%M:%S') -> str:
+    return datetime.now().strftime(fmt)
+
+if __name__ == '__main__':
+    os.makedirs("data/logs", exist_ok=True)
+    os.makedirs("data/prices", exist_ok=True)
+
+    kospi_codes = stock.get_market_ticker_list(datetime.today().strftime('%Y%m%d'), market="KOSPI")
+    kosdaq_codes = stock.get_market_ticker_list(datetime.today().strftime('%Y%m%d'), market="KOSDAQ")
+
+    codes_dict = {
+        "KOSPI" : kospi_codes,
+        "KOSDAQ" : kosdaq_codes
+    }
+    duration_minutes = 2 #60 * 24 # 실행하고 싶은 시간(단위:분)
+
+    create_stock_data(codes_dict,duration_minutes)
